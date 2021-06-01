1c293610aa091c5f9f4aeae30f0b99b005d607d3
Behzad <behzad.khosravifar@gmail.com>
2021-06-02 00:17:03 +0430

    Refactore duplicate methods

--- src/TestMultipleFileWriter/TestMultipleFileWriter/Program.cs
+++ src/TestMultipleFileWriter/TestMultipleFileWriter/Program.cs
@@ -13,7 +13,7 @@ namespace TestMultipleFileWriter
         private const string MapperName = "TestMapper";
 
         private const int NumberOfStreams = 4;
-        private const long TotalLength = 1024L * 1024 * 1024; // 1GB
+        private const long TotalLength = 6 * 1024L * 1024 * 1024; // 6GB
         private const long ChunkLength = TotalLength / NumberOfStreams;
         private static List<Range> Ranges = new List<Range>();
 
@@ -23,9 +23,10 @@ namespace TestMultipleFileWriter
             {
                 SetRanges();
 
-                await WriteConsole("Writing", CreateDummyHugeFile(MapperName));
-                await WriteConsole("Testing", TestDummyHugeFile());
-                await WriteConsole("Re-sampling", ReSamplingDummyHugeFile(MapperName));
+                await WriteConsole("Writing", CreateDummyHugeFile(false));
+                await WriteConsole("Testing", TestDummyHugeFile(false));
+
+                await WriteConsole("Re-Sampling", CreateDummyHugeFile(true));
                 await WriteConsole("Testing", TestDummyHugeFile(true));
             }
             catch (Exception e)
@@ -41,23 +42,18 @@ namespace TestMultipleFileWriter
             Console.ReadKey();
         }
 
-        private static async Task WriteConsole(string title, Task act)
-        {
-            Console.Write($"{title} File ({TotalLength} bytes)");
-            var sw = Stopwatch.StartNew();
-            await act;
-            sw.Stop();
-            Console.WriteLine($" took {sw.ElapsedMilliseconds} milliseconds");
-        }
         private static async Task WriteConsole(string title, Task<bool> act)
         {
             Console.Write($"{title} File ({TotalLength} bytes)");
+            await Task.Delay(100);
+
             var sw = Stopwatch.StartNew();
-            var test = await act;
+            var result = await act;
             sw.Stop();
+
             Console.Write($" took {sw.ElapsedMilliseconds} milliseconds ");
 
-            if (test)
+            if (result)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Passed");
@@ -70,7 +66,7 @@ namespace TestMultipleFileWriter
 
             Console.ResetColor();
         }
-        private static async Task<bool> TestDummyHugeFile(bool isReversed = false)
+        private static async Task<bool> TestDummyHugeFile(bool isReversed)
         {
             await using var reader = File.OpenRead(path);
 
@@ -101,41 +97,25 @@ namespace TestMultipleFileWriter
             }
         }
 
-        static async Task CreateDummyHugeFile(string mapperName)
+        static async Task<bool> CreateDummyHugeFile(bool isReversed)
         {
             //Ranges list populated here
-            var fileMapper = MemoryMappedFile.CreateFromFile(path, FileMode.OpenOrCreate, mapperName,
+            var fileMapper = MemoryMappedFile.CreateFromFile(path, FileMode.OpenOrCreate, MapperName,
                 NumberOfStreams * ChunkLength, MemoryMappedFileAccess.ReadWrite);
 
             var tasks = new Task[NumberOfStreams];
             for (var i = 0; i < NumberOfStreams; i++)
             {
                 var index = i;
+                var data = isReversed ? NumberOfStreams - index : index;
                 var fileStream = fileMapper.CreateViewStream(Ranges[index].Start, ChunkLength, MemoryMappedFileAccess.Write);
-                tasks[i] = DoWriting(fileStream, index);
+                tasks[i] = DoWriting(fileStream, data);
             }
 
             fileMapper.Dispose(); // Note: I can to dispose mapper before start view streams
             await Task.WhenAll(tasks);
-        }
 
-        static async Task ReSamplingDummyHugeFile(string mapperName)
-        {
-            //Ranges list populated here
-            var fileMapper = MemoryMappedFile.CreateFromFile(path, FileMode.OpenOrCreate, mapperName,
-                NumberOfStreams * ChunkLength, MemoryMappedFileAccess.ReadWrite);
-
-            var tasks = new Task[NumberOfStreams];
-            for (var i = 0; i < NumberOfStreams; i++)
-            {
-                var index = i;
-                var data = NumberOfStreams - index;
-                var fileStream = fileMapper.CreateViewStream(Ranges[index].Start, ChunkLength, MemoryMappedFileAccess.Write);
-                tasks[i] = Task.Factory.StartNew(() => DoWriting(fileStream, data));
-            }
-
-            fileMapper.Dispose(); // Note: I can to dispose mapper before start view streams
-            await Task.WhenAll(tasks);
+            return true;
         }
 
         static async Task DoWriting(Stream fileStream, int data, int bufferSize = 1024)
